3fb8b93bd7356ce013790adc0459f75044383f53
Aneeta <aneetatomy23@gmail.com>
2016-07-06 21:52:40 +0530

    callbacks included

--- blog/app/models/article.rb
+++ blog/app/models/article.rb
@@ -1,2 +1,21 @@
 class Article < ActiveRecord::Base
+    has_many :comments, dependent: :destroy
+    
+	validates :title, presence: true,
+            length: { minimum: 5 }
+    
+
+     before_save :send_text
+     
+
+     def send_text 
+       self.text = self.title if text.blank?
+     end
+
+     after_save :send_message
+
+     def send_message
+      puts "saved successfully"
+     end 
+
 end
